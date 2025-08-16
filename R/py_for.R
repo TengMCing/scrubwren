323055fabe411aa962498c67428b441af63d5748
Weihao Li <llreczx@gmail.com>
2025-08-16 16:55:31 +1000

    Add py_for

---
+++ R/py_for.R
@@ -0,0 +1,23 @@
+#' @param envir where to run the for loop.
+#' @export
+py_for <- function(loop_spec, body, envir = parent.frame()) {
+  if (!is.call(loop_spec) || loop_spec[[1]] != as.symbol("~"))
+    cli::cli_abort("The loop specification needs to be a formula!")
+  
+  var_sym <- loop_spec[[2]]
+  iter_sym <- loop_spec[[3]]
+  iter <- eval(iter_sym, envir = envir)
+  
+  # If `iter` only implements the __iter__ method but not the __next__ method,
+  # we need to convert it to a iterator.
+  if (!reticulate::py_to_r(py_builtins$hasattr(iter, "__next__"))) {
+    iter <- py_builtins$iter(iter)  
+  }
+  
+  while (TRUE) {
+    item <- reticulate::iter_next(iter, completed = quote(StopIteration))
+    if (identical(item, quote(StopIteration))) break
+    py_tuple_unpack(var_sym, item, envir = envir, quote_vars = FALSE)
+    eval(substitute(body), envir = envir)
+  }
+}
--- R/py_tuple_unpack.R
+++ R/py_tuple_unpack.R
@@ -1,5 +1,5 @@
-.scrubwren_state <- new.env()
 
+#' @param envir where to assign the value to vars.
 #' @export
 py_tuple_unpack <- function(vars, value, envir = parent.frame(), quote_vars = TRUE) {
   if (quote_vars) {
