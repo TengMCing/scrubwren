6192c69834d4297460d74c20bc3c5a94980d46d0
Weihao Li <llreczx@gmail.com>
2025-08-16 15:53:15 +1000

    Add py_unpack

---
+++ R/py_unpack.R
@@ -0,0 +1,31 @@
+.scrubwren_state <- new.env()
+
+#' @export
+py_unpack <- function(vars, value, envir = parent.frame(), quote_vars = TRUE) {
+  if (quote_vars) {
+    vars <- substitute(vars)
+  }
+  
+  if (!is.call(vars) || vars[[1]] != as.symbol("c")) {
+    .scrubwren_state$last_unpack_value <- value
+    expr <- bquote(.(vars) <- .scrubwren_state$last_unpack_value)
+    eval(expr, envir = envir)
+    return(invisible(NULL))
+  }
+  
+  if (reticulate::is_py_object(value)) {
+    if (reticulate::py_to_r(py_builtins$len(value)) > length(vars) - 1) 
+      cli::cli_warn("Unpacked values remain: the variable structure {.var {deparse(vars)}} is shorter than expected [{reticulate::py_to_r(py_builtins$len(value))}]!")
+  } else {
+    if (length(value) > length(vars) - 1) 
+      cli::cli_warn("Unpacked values remain: the variable structure {.var {deparse(vars)}} is shorter than expected [{length(value)}]!")
+  }
+  
+  for (i in 2:length(vars)) {
+    if (reticulate::is_py_object(value)) {
+      py_unpack(vars[[i]], value[[i - 2]], envir, FALSE)
+    } else {
+      py_unpack(vars[[i]], value[[i - 1]], envir, FALSE)
+    }
+  }
+}
