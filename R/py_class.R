9e0f7bff42a071eb18ee42c80742be9e9b705546
Weihao Li <llreczx@gmail.com>
2025-08-17 22:19:06 +1000

    Document `py_class()`

--- R/py_class.R
+++ R/py_class.R
@@ -1,3 +1,30 @@
+#' Create a Python class
+#' 
+#' This function works like [reticulate::PyClass()], but the `defs` argument 
+#' can now be provided as individual arguments (`...`) instead of a list.
+#' 
+#' @param classname Character. Name of the class.
+#' @param ... Functions, attributes, or other definitions for the class.
+#' @param inherit List. A list of Python class objects representing the 
+#' parent classes.
+#' 
+#' @examples
+#' 
+#' \dontrun{
+#' Employee <- py_class("Employee", 
+#'                      `__init__` = function(self, name, id) {
+#'                        self$name <- name
+#'                        self$id <- id
+#'                        return(invisible(NULL))
+#'                      },
+#'                      get_email = function(self) {
+#'                        paste0(self$name, "_", self$id, "@company.com")
+#'                      })
+#' Mike <- Employee("Mike", "1234")
+#' Mike$get_email()
+#' }
+#' 
+#' 
 #' @export
 py_class <- function(classname, ..., inherit = NULL) {
   expr <- substitute(reticulate::PyClass(classname = classname, 
@@ -5,3 +32,6 @@ py_class <- function(classname, ..., inherit = NULL) {
                                          inherit = inherit))
   eval.parent(expr)
 }
+
+
+
