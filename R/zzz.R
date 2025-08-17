377a64b03e610db63b3fabefc3ce5cad5a68c25c
Weihao Li <llreczx@gmail.com>
2025-08-17 21:37:22 +1000

    Document `py_builtins`

--- R/zzz.R
+++ R/zzz.R
@@ -5,9 +5,16 @@ py_builtins_bindings <- function(x) {
     # Delay the import of builtins to give the user the opportunity to 
     # declare which python interpreter to use.
     if (is.null(.scrubwren_state$py_builtins)) {
-      cli::cli_alert_info("Importing `py_builtins` from {.field Python {reticulate::py_config()$version}} at {.file {reticulate::py_config()$python}}.")
-      cli::cli_alert_info("You can re-import `py_builtins` by calling `reimport_py_builtins()` after setting the interpreter with `reticulate::use_python()`.")
-      .scrubwren_state$py_builtins <- reticulate::import_builtins(convert = FALSE) 
+      
+      potential_py <- reticulate::py_discover_config()
+      
+      if (is.null(potential_py)) {
+        cli::cli_alert_danger("No Python interpreter could be detected! Please install Python and set the interpreter using `reticulate::use_python()`.")
+      } else {
+        cli::cli_alert_info("Importing `py_builtins` from {.field Python {reticulate::py_config()$version}} at {.file {reticulate::py_config()$python}}.")
+        cli::cli_alert_info("You can re-import `py_builtins` by calling `reimport_py_builtins()` after setting the interpreter with `reticulate::use_python()`.")
+        .scrubwren_state$py_builtins <- reticulate::import_builtins(convert = FALSE) 
+      }
     }
   }
   
@@ -21,7 +28,13 @@ reimport_py_builtins <- function(convert = FALSE) {
   .scrubwren_state$py_builtins <- reticulate::import_builtins(convert = convert) 
 }
 
-#' List objects
+#' Python's built-in functions.
+#' 
+#' These built-in functions are imported when the package loads. 
+#' By default, `reticulate::py_discover_config()` is used to determine the Python version. 
+#' To use a different Python interpreter, first set it with `reticulate::use_python()`, 
+#' then re-import the built-in functions by calling `reimport_py_builtins()`.
+#' 
 #' @usage py_builtins
 #' @name py_builtins
 #' @export
