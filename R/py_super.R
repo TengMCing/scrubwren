1f809607c4a7efcabfc416d644116250fca982e7
Weihao Li <llreczx@gmail.com>
2025-08-17 23:56:59 +1000

    Add doc for `py_super_init()`, remove `py_super()` as one can use `super()` directly

--- R/py_super.R
+++
@@ -1,9 +0,0 @@
-#' @export
-py_super <- function() {
-  eval.parent(quote(py_builtins$super(`__class__`, self)))
-}
-
-#' @export
-py_super_init <- function(...) {
-  eval.parent(substitute(py_builtins$super(`__class__`, self)$`__init__`(...)))
-}
---
+++ R/py_super_init.R
@@ -0,0 +1,42 @@
+#' Call Python's `super().__init__()` from R
+#' 
+#' This function mimics Python's built-in `super().__init__()` method, 
+#' calling the initializer of the superclass for the current object.
+#' It is equivalent to ``super()$`__init__`(...)``.
+#'
+#' @param ... Arguments to pass to the superclass initializer.
+#' @return No return; called for side effect.
+#' @examples
+#' \dontrun{
+#' Employee <- py_class("Employee",
+#'                      `__init__` = function(self, name, id) {
+#'                        self$name <- name
+#'                        self$id <- id
+#'                        return(py_builtins$None)
+#'                      },
+#'                      get_email = function(self) {
+#'                        paste0(self$name, "_", self$id, "@company.com")
+#'                      })
+#' 
+#' Salary <- py_class("Salary", inherit = Employee,
+#'                    `__init__` = function(self, name, id, salary) {
+#'                      super()$`__init__`(name, id)
+#'                      self$salary <- salary
+#'                      return(py_builtins$None)
+#'                    },
+#'                    get_salary_summary = function(self) {
+#'                      list(ID = self$id,
+#'                           Name = self$name,
+#'                           Email = self$get_email(),
+#'                           Salary = self$salary)
+#'                    })
+#' 
+#' mike_salary <- Salary("Mike", "1234", 1000)
+#' mike_salary$get_salary_summary()
+#' mike_salary$get_email()
+#' }
+#' 
+#' @export
+py_super_init <- function(...) {
+  eval.parent(substitute(super()$`__init__`(...)))
+}
