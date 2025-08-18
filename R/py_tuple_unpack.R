16c7ba3b89f377b13dbbc0845d1e5ef6a7c540f0
Weihao Li <llreczx@gmail.com>
2025-08-18 13:26:53 +1000

    Doc `py_tuple_unpack()`

--- R/py_tuple_unpack.R
+++ R/py_tuple_unpack.R
@@ -1,5 +1,50 @@
-
-# @param envir where to assign the value to vars.
+#' Unpack a Python tuple (or sequence) into R variables
+#'
+#' This function unpacks elements of a Python tuple (or any sequence-like
+#' object) into R variables, similar to tuple unpacking in Python
+#' (`a, b = (1, 2)`).  
+#'
+#' If `vars` is a vector of names created with `c(...)`, each element of
+#' `value` is assigned to the corresponding variable in the calling
+#' environment. Nested unpacking is supported recursively.
+#'
+#' @param vars A symbol or a call like `c(a, b, c)` representing the
+#'   variables to assign into.
+#' @param value A Python object (tuple, list, etc.) or an R vector/list to
+#'   unpack.
+#' @param envir Environment. Environment in which to assign the unpacked values. 
+#' Defaults to the calling environment.
+#' @param quote_vars Boolean. If `TRUE`, `vars` is captured unevaluated
+#'   (recommended when writing `c(a, b, c)` directly).
+#'
+#' @return Invisibly returns `NULL`. Called for side effects.
+#'
+#' @details
+#' If the number of elements in `value` exceeds the number of variables
+#' provided, a warning is issued but unpacking proceeds for the available
+#' variables.
+#'
+#' @examples
+#' \dontrun{
+#' # Simple unpacking
+#' py_tuple_unpack(c(a, b), list(1, 2))
+#' a  # 1
+#' b  # 2
+#'
+#' # With nested unpacking
+#' py_tuple_unpack(c(a, c(b, d)), list(1, list(2, 3)))
+#' a  # 1
+#' b  # 2
+#' d  # 3
+#'
+#' # Works with Python tuples/lists
+#' tup <- reticulate::tuple(list(10, list(20, 30)))
+#' py_tuple_unpack(c(x, c(y, z)), tup)
+#' x  # 10
+#' y  # 20
+#' z  # 30
+#' }
+#'
 #' @export
 py_tuple_unpack <- function(vars, value, envir = parent.frame(), quote_vars = TRUE) {
   if (quote_vars) {
@@ -8,7 +53,7 @@ py_tuple_unpack <- function(vars, value, envir = parent.frame(), quote_vars = TR
   
   if (!is.call(vars) || vars[[1]] != as.symbol("c")) {
     .scrubwren_state$last_tuple_unpack_value <- value
-    expr <- bquote(.(vars) <- .scrubwren_state$last_tuple_unpack_value)
+    expr <- bquote(.(vars) <- get_scrubwren_state()$last_tuple_unpack_value)
     eval(expr, envir = envir)
     return(invisible(NULL))
   }
