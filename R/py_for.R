9021dd6dd2edbf4ae5d3f5081013072ab962b195
Weihao Li <llreczx@gmail.com>
2025-08-18 12:41:04 +1000

    Add `py_is_iterable()` and `py_is_iterator()`

--- R/py_for.R
+++ R/py_for.R
@@ -1,3 +1,24 @@
+#' @export
+py_is_iterable <- function(obj) {
+  reticulate::py_config()
+  if (!reticulate::is_py_object(obj)) cli::cli_abort("{substitute(obj)} is not a Python object!")
+  result <- tryCatch(py_builtins$iter(obj),
+                     python.builtin.TypeError = function(e) quote(TypeError))
+  if (identical(result, quote(TypeError))) {
+    return(FALSE)
+  } else {
+    return(TRUE)
+  }
+}
+
+#' @export
+py_is_iterator <- function(obj) {
+  if (!reticulate::is_py_object(obj)) cli::cli_abort("{substitute(obj)} is not a Python object!")
+  collections <- reticulate::import(collections, convert = FALSE)
+  result <- py_buiitins$isinstance(obj, collections$abc$Iterator) 
+  return(reticulate::py_to_r(result))
+}
+
 # @param envir where to run the for loop.
 #' @export
 py_for <- function(loop_spec, body, envir = parent.frame()) {
@@ -10,8 +31,8 @@ py_for <- function(loop_spec, body, envir = parent.frame()) {
   
   # If `iter` only implements the __iter__ method but not the __next__ method,
   # we need to convert it to a iterator.
-  if (!reticulate::py_has_attr(iter, "__next__")) {
-    iter <- py_builtins$iter(iter)  
+  if (!py_is_iterator(iter)) {
+    iter <- reticulate::as_iterator(iter)  
   }
   
   while (TRUE) {
